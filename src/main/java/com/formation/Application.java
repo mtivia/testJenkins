e60451f2949079fc625db2f5acf1dd752270f2d4
mtivia <mtivia@gmail.com>
2018-09-05 10:45:56 +0200

    test 2

--- src/main/java/com/formation/Application.java
+++ src/main/java/com/formation/Application.java
@@ -3,5 +3,6 @@ package com.formation;
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!!");
+		System.out.println("deuxieme test !!!!!!!!!!");
 	}
 }
